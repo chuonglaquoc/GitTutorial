10b37d3dcd188a8c2f79c8f80a22ba571f35c8e2
Brian La <chuonglaquoc100@gmail.com>
2021-09-01 02:16:18 +0700

    xzczxczxczxc

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Brian La on 01/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Git Tutorial")
+    }
+
+
+}
+
